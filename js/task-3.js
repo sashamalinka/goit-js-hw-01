ec8b85b97086bb7668fb937b784541d546e6f512
Sasha <malinka15117kk@gmail.com>
2024-12-28 10:16:57 +0200

    fixborder

--- js/task-3.js
+++ js/task-3.js
@@ -1,7 +1,7 @@
 function getElementWidth(content, padding, border) {
     const contentWidth = parseFloat(content);
     const paddingWidth = parseFloat(padding) * 2;
-    const borderWidth = parseFloat(boder) * 2;
+    const borderWidth = parseFloat(border) * 2;
 
     return = contentWidth + paddingWidth + borderWidth;
 }
